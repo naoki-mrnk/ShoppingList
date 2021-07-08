5039d75154dfa75a23fdc606b4aac6670a38c7e1
naoki-mrnk <20cm0135@jec.ac.jp>
2021-07-08 11:06:07 +0900

    ボタンの処理を追加

--- ShoppingList/Controller/ShoppingListViewController.swift
+++ ShoppingList/Controller/ShoppingListViewController.swift
@@ -9,10 +9,16 @@ import UIKit
 
 class ShoppingListViewController: UIViewController {
 
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var shoppingListTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        addButton.settingButton()
     }
-
+    
+    
+    @IBAction func tappedAddButton(_ sender: UIButton) {
+        performSegue(withIdentifier: "toAddNewItem", sender: nil)
+    }
 }
---
+++ ShoppingList/Extension/SettingButton.swift
@@ -0,0 +1,14 @@
+//
+//  SettingButton.swift
+//  ShoppingList
+//
+//  Created by naoki-mrnk on 2021/07/08.
+//
+
+import UIKit
+
+extension UIButton {
+    func settingButton() {
+        layer.cornerRadius = 25
+    }
+}
