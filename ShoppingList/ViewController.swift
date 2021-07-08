c5e33835ccb5435e11279f3beb6fcf97dc3d8c22
naoki-mrnk <20cm0135@jec.ac.jp>
2021-07-08 09:46:46 +0900

    Initial Commit

---
+++ ShoppingList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoppingList
+//
+//  Created by cmStudent on 2021/07/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
